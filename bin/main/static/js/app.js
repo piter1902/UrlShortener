9ab1b20b58ec41321f7149a3810626b85f62e74b
jtambo99 <jtambo@live.com>
2020-11-05 18:45:50 +0100

    Commit de conflictos. No es importante

--- bin/main/static/js/app.js
+++ bin/main/static/js/app.js
@@ -13,10 +13,14 @@ $(document).ready(
                             + msg.uri
                             + "'>"
                             + msg.uri
+<<<<<<< Updated upstream
                             + "</a><p>Safeness: " + msg.safe + "</p>"
 //                            + "<img src=\"data:image/png;base64, " + msg.qrCode + "\" />"
                             + "<img src=\"" + msg.qrCode + "\" />"
                             + "</div>" );
+=======
+                            + "</a></div>");
+>>>>>>> Stashed changes
                     },
                     error: function () {
                         $("#result").html(

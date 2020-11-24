756c7934495ea67bf3f9b18bbc2ae777652b58e1
hayk99 <haykk1999@gmail.com>
2020-11-24 09:47:12 +0100

    test passing and i think multimodule project is ok

---
+++ workers/src/main/java/workers/App.java
@@ -0,0 +1,34 @@
+package workers;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import urlshortener.messagingrabbitmq.Sender;
+
+@SpringBootApplication
+public class App extends SpringBootServletInitializer implements CommandLineRunner {
+
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    Sender sender;
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(App.class);
+    }
+
+    @Override
+    public void run(String... args) throws InterruptedException {
+        sender.send("mensaje desde application");
+    }
+}

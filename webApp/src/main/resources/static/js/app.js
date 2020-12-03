378083b93fbdd2b9406257a37edd374ed626f649
jtambo99 <jtambo@live.com>
2020-12-03 17:59:32 +0100

    Replaced ajax request with XMLHttpRequest on csv petitions

--- webApp/src/main/resources/static/js/app.js
+++ webApp/src/main/resources/static/js/app.js
@@ -24,35 +24,74 @@ $(document).ready(
                     }
                 });
             });
-         //Tutorial: https://attacomsian.com/blog/spring-boot-file-upload-with-ajax
-        $("#file-upload-form").on("submit", function(e){
-            e.preventDefault();
-            $.ajax({
-                url: "/uploadCSV",
-                type: "POST",
-                data: new FormData(this),
-                //enctype: 'multipart/form-data',
-                processData: false,
-                contentType: false,
-                cache: false,
-                success: function(data){
-                    console.log("RESULTADO:");
-                    console.log(data);
-                    $.ajax({
-                        url: "/files/"+data,
-                        type:"GET",
-                        success: function(res){
-                            var blob = new Blob([res]);
-                            var link=document.createElement('a');
-                            link.href=window.URL.createObjectURL(blob);
-                            link.download="shorted_urls.csv";
-                            link.click();
-                        }
-                    });
-                },
-                error: function(err) {
-                    console.error(err);
-                }
+            // Tutorial https://www.accelebrate.com/blog/web-page-file-uploads-ajax-part-2-of-3/
+            $("#file-upload-form").on("submit", function(e){
+                e.preventDefault();
+
+                // Create a new FormData object to hold the file datavar
+                fd = new FormData();
+                // Add the data from the file input field to the FormData object
+                fd.append("file", document.getElementById("file-upload-input").files[0]);
+                // Initialize a new XHR object
+                var xhr = new XMLHttpRequest();
+                // handle ready state change events
+                xhr.onreadystatechange = function() {
+
+                  if (xhr.readyState === 4 && xhr.status === 201) {
+                    // upload is complete, output result data
+                    console.log(xhr.responseText);
+                    downloadFile(xhr.responseText);
+                  }
+                };
+                 // configure the request
+                  xhr.open("POST", "/uploadCSV");
+                  // makes the request and uploads the form data
+                  xhr.send(fd);
             });
-        });
+
+            function downloadFile(fileName){
+                var xhr = new XMLHttpRequest();
+                xhr.onreadystatechange = function() {
+                    if (this.readyState == 4 && this.status == 200) {
+                        var blob = new Blob([xhr.response]);
+                        var link=document.createElement('a');
+                        link.href=window.URL.createObjectURL(blob);
+                        link.download="shorted_urls.csv";
+                        link.click();
+                    }
+                  };
+                  xhr.open("GET", "/files/"+fileName, true);
+                  xhr.send();
+            }
+         //Tutorial: https://attacomsian.com/blog/spring-boot-file-upload-with-ajax
+//        $("#file-upload-form").on("submit", function(e){
+//            e.preventDefault();
+//            $.ajax({
+//                url: "/uploadCSV",
+//                type: "POST",
+//                data: new FormData(this),
+//                //enctype: 'multipart/form-data',
+//                processData: false,
+//                contentType: false,
+//                cache: false,
+//                success: function(data){
+//                    console.log("RESULTADO:");
+//                    console.log(data);
+//                    $.ajax({
+//                        url: "/files/"+data,
+//                        type:"GET",
+//                        success: function(res){
+//                            var blob = new Blob([res]);
+//                            var link=document.createElement('a');
+//                            link.href=window.URL.createObjectURL(blob);
+//                            link.download="shorted_urls.csv";
+//                            link.click();
+//                        }
+//                    });
+//                },
+//                error: function(err) {
+//                    console.error(err);
+//                }
+//            });
+//        });
     });
